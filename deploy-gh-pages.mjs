c585ea5556d8b5a5472bf5489c4f302f14050d3a
Max Base (Seyyed Ali Mohammadiyeh) <MaxBaseCode@gmail.com>
2025-05-21 11:30:11 +0330

    Add deploy-gh-pages.mjs

---
+++ deploy-gh-pages.mjs
@@ -0,0 +1,59 @@
+/* eslint-disable max-statements, no-process-exit, no-console */
+import { execa } from "execa";
+import * as fs from "fs";
+
+(async () => {
+  try {
+    await execa("git", [
+      "checkout",
+      "--orphan",
+      "gh-pages",
+    ]);
+    console.log(`Building started...`);
+    await execa("npm", [
+      "run",
+      "build",
+    ]);
+
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", [
+      "--work-tree",
+      folderName,
+      "add",
+      "--all",
+    ]);
+    await execa("git", [
+      "--work-tree",
+      folderName,
+      "commit",
+      "-m",
+      "gh-pages",
+    ]);
+    console.log(`Pushing to gh-pages...`);
+    await execa("git", [
+      "push",
+      "origin",
+      "HEAD:gh-pages",
+      "--force",
+    ]);
+    await execa("rm", [
+      "-r",
+      folderName,
+    ]);
+    await execa("git", [
+      "checkout",
+      "-f",
+      "master",
+    ]);
+    await execa("git", [
+      "branch",
+      "-D",
+      "gh-pages",
+    ]);
+    console.log(`Successfully deployed`);
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
